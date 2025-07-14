623f4e51354caeb1c3e3f3d33e04e61261e9ead9
WOLF003 <philip.van.der.wolf@persgroep.net>
2025-07-14 20:43:59 +0200

    Update build.

--- _build/new_build.ps1
+++ _build/new_build.ps1
@@ -3,3 +3,15 @@ RPCL3_GAME_OVER WAVE "C:\\repos\\rpcl3-process-control\\_build\\rpcl3_media\\RPC
 RPCL3_DEFAULT_256 PNG "C:\\repos\\rpcl3-process-control\\_build\\rpcl3_media\\RPCL3_DEFAULT_256.png"
 
 
+$rcContent = @()
+foreach ($wav in $wavFiles) {
+    $wavPath = (Resolve-Path $wav -ErrorAction Stop).Path
+    $resName = [System.IO.Path]::GetFileNameWithoutExtension($wav).ToUpper()
+    $rcContent += "$resName WAVE `"$wavPath`""
+}
+foreach ($png in $pngFile) {
+    $pngPath = (Resolve-Path $png).Path
+    $resName = [System.IO.Path]::GetFileNameWithoutExtension($png).ToUpper()
+    $rcContent += "$resName PNG `"$pngPath`""
+}
+$rcContent | Out-File "add_media.rc" -Encoding ASCII
