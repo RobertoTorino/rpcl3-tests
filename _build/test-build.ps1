3c07ac2230aae35be0ada609f390348b68c14dde
WOLF003 <philip.van.der.wolf@persgroep.net>
2025-07-14 22:19:35 +0200

    Update build.

--- _build/test-build.ps1
+++ _build/test-build.ps1
@@ -8,8 +8,23 @@ $resHacker  = "C:\Program Files (x86)\Resource Hacker\ResourceHacker.exe"
 $rcPath     = "C:\TestEmbed\add_media.rc"
 $logPath    = "C:\TestEmbed\build.log"
 
+Remove-Item 'C:\TestEmbed\add_media.rc' -ErrorAction SilentlyContinue
+Remove-Item 'C:\TestEmbed\withsound.exe' -ErrorAction SilentlyContinue
+Remove-Item 'C:\TestEmbed\build.log' -ErrorAction SilentlyContinue
+
+Test-Path 'C:\TestEmbed\source.exe'      # Should be True
+Test-Path 'C:\TestEmbed\alert.wav'       # Should be True
+
 # Write RC file (no blank lines)
-[System.IO.File]::WriteAllText($rcPath, 'ALERT_SOUND WAVE "' + $wav + '"', [System.Text.Encoding]::ASCII)
+[System.IO.File]::WriteAllText(
+        'C:\TestEmbed\add_media.rc',
+        'ALERT_SOUND WAVE "C:\TestEmbed\alert.wav"',
+        [System.Text.Encoding]::ASCII
+)
+
+Get-Content 'C:\TestEmbed\add_media.rc'
+
+[System.IO.File]::ReadAllBytes('C:\TestEmbed\add_media.rc') | % { "{0:X2}" -f $_ } | -join ' '
 
 # Build Resource Hacker command
 $rhArgs = @(
