bc2fa15874cbe1c5b51cc7645e4e33a688975cb7
WOLF003 <philip.van.der.wolf@persgroep.net>
2025-07-16 22:53:58 +0200

    Update build.

--- _build/test-build.ps1
+++ _build/test-build.ps1
@@ -1,24 +1,18 @@
-; AutoHotkey portion to embed assets
 FileInstall, rpcl3_media\RPCL3_GOOD_MORNING.wav, %A_Temp%\RPCL3_GOOD_MORNING.wav
 FileInstall, rpcl3_media\RPCL3_GAME_OVER.wav, %A_Temp%\RPCL3_GAME_OVER.wav
 FileInstall, rpcl3_media\RPCL3_DEFAULT_256.png, %A_Temp%\RPCL3_DEFAULT_256.png
 
-; In your code you can now use:
-; %A_Temp%\RPCL3_GOOD_MORNING.wav etc
-MsgBox, WAV will be at: %A_Temp%\RPCL3_GOOD_MORNING.wav
 
 
 
-
-# Build FileInstall lines for your .ahk
-$fileInstallLines = @()
-foreach ($wav in $wavFiles) {
-    $assetName = Split-Path $wav -Leaf
-    $fileInstallLines += "FileInstall, $wav, `%A_Temp`%\\$assetName"
+defaultIcon := A_Temp . "\RPCL3_DEFAULT_256.png"
+if FileExist(defaultIcon) {
+    Gui, Add, Picture, x580 y22 w65 h65 vRPCL3Icon, %defaultIcon%
 }
-foreach ($png in $pngFile) {
-    $assetName = Split-Path $png -Leaf
-    $fileInstallLines += "FileInstall, $png, `%A_Temp`%\\$assetName"
-}
-Write-Host "`n# Copy-paste these lines to your AHK script:`n"
-$fileInstallLines | ForEach-Object { Write-Host $_ }
+
+
+wav := A_Temp . "\RPCL3_GOOD_MORNING.wav"
+if FileExist(wav)
+SoundPlay, %wav%
+else
+MsgBox, WAV not found at: %wav%
